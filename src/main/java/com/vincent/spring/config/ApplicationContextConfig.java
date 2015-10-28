ef994e97e2c1fefd6f728d046b748cda80d2391e
Vincent Zheng <zhengye1@mail.utoronto.ca>
2015-10-28 00:47:21 -0400

    forgot some of the files...

---
+++ src/main/java/com/vincent/spring/config/ApplicationContextConfig.java
@@ -0,0 +1,76 @@
+package com.vincent.spring.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.vincent.spring.dao.UserDAO;
+import com.vincent.spring.dao.UserDAOImpl;
+import com.vincent.spring.model.User;
+
+@Configuration
+@ComponentScan("com.vincent.spring")
+@EnableTransactionManagement
+
+public class ApplicationContextConfig {
+	@Bean(name="viewResolver")
+	public InternalResourceViewResolver getViewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean(name="dataSource")
+	public DataSource getDataSource(){
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/usersdb");
+		dataSource.setUsername("root");
+		dataSource.setPassword("Yukirin0715");
+		return dataSource;
+	}
+    
+	private Properties getHibernateProperties() {
+    	Properties properties = new Properties();
+    	properties.put("hibernate.show_sql", "true");
+    	properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+    	return properties;
+    }
+	
+	@Autowired
+	@Bean(name="sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource){
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+		sessionBuilder.addAnnotatedClasses(User.class);
+		return sessionBuilder.buildSessionFactory();
+	}
+	
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(
+	        SessionFactory sessionFactory) {
+	    HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+	            sessionFactory);
+	 
+	    return transactionManager;
+	}
+	
+	@Autowired
+	@Bean(name = "userDao")
+	public UserDAO getUserDao(SessionFactory sessionFactory) {
+	    return new UserDAOImpl(sessionFactory);
+	}
+}
